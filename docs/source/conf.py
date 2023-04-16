d27302fb35e990098b9625b32ba962c720af7c6a
AnonymouX47 <anonymoux47@gmail.com>
2023-04-16 15:21:29 +0100

    docs: Cleanup docs config
    - Change: Remove unneeded and unused options.
    - Change: Update comments.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of configuration options, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import argparse
@@ -11,6 +10,7 @@ from docutils.statemachine import StringList
 from sphinx_toolbox import confval
 from sphinxcontrib import autoprogram
 
+# Sets the attributes removed in the CLI Parser section below
 from termvisage import parsers  # noqa: F401
 from termvisage import __version__
 
@@ -21,46 +21,21 @@ from termvisage import __version__
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath("../../src"))
 
-
 # -- Project information -----------------------------------------------------
-
 project = "TermVisage"
 copyright = "2023, Toluwaleke Ogundipe"
 author = "Toluwaleke Ogundipe"
 release = __version__
 
-
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_toolbox.confval",
     "sphinxcontrib.autoprogram",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = []
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 html_theme = "furo"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
-
 
 # -- Options for extensions ----------------------------------------------
 
@@ -69,11 +44,6 @@ intersphinx_mapping = {
     "term_image": ("https://term-image.readthedocs.io/en/v0.6.0/", None),
 }
 intersphinx_disabled_reftypes = ["*"]
-
-
-# -- Others options ----------------------------------------------------------
-toc_object_entries = False
-
 
 # -- Extras -----------------------------------------------------------------
 
@@ -111,6 +81,7 @@ class ConfigValue(confval.ConfigurationValue, ConfigValueSibling):
 confval.ConfigurationValue = ConfigValue
 
 # # -- CLI Parser -----------------------------------------------------------
+
 # Do not strip reST markup
 del argparse.ArgumentParser.epilog
 del argparse.Action.help
