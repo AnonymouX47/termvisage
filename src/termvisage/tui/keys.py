AnonymouX47/termvisage

commit a2440484b36621138cda853cdcce9faf0ac569e1

feat: Re-render grid cells upon window resize
- Change: Clear grid render cache when the terminal is resized.
  - Eliminates wrong scaling and resolution when the terminal is zoomed.

src/termvisage/tui/keys.py
@@ -244,7 +244,7 @@ def display_context_keys(context: str) -> None:
             if visible
         ]
     )
-    resize()
+    adjust_bottom_bar()
 
 
 def register_key(*args: Tuple[str, str]) -> FunctionType:
@@ -349,7 +349,7 @@ def help():
     getattr(main.ImageClass, "clear", lambda: True)()
 
 
-def resize():
+def adjust_bottom_bar():
     cols = get_terminal_size()[0]
     rows = key_bar.original_widget.rows((cols,))
     if expand._ti_shown:
@@ -379,6 +379,15 @@ def key_bar_rows():
         - (len(expand.original_widget.text) + 2) * expand._ti_shown
     )
     return key_bar.original_widget.rows((cols,))
+
+
+def resize():
+    adjust_bottom_bar()
+    main.grid_render_queue.put(None)  # Mark the start of a new grid
+    main.grid_change.set()
+    # Wait till GridRenderManager clears the cache
+    while main.grid_change.is_set():
+        pass
 
 
 keys["global"].update({"resized": [resize, True]})

src/termvisage/tui/__init__.py
@@ -142,7 +142,7 @@ class Loop(urwid.MainLoop):
 
     def process_input(self, keys):
         if "window resize" in keys:
-            # Adjust bottom bar upon window resize
+            # Adjust bottom bar and clear grid cache upon window resize
             keys.append("resized")
             getattr(main.ImageClass, "clear", lambda: True)() or ImageCanvas.change()
         return super().process_input(keys)

commit 4bb9a7cce6aa1bb82bf86da5049393e1ccc558de

fix: Clear grid cache only when a grid is active
- Fix: Clear the grid render cache only when the grid is active since
  the GridRenderer thread typically blocks while the grid is inactive.

  - Fix deadlock at TUI init without a directory source.
  - Fix deadlock when the terminal is resized and a grid is not active.

Broken in a2440484b36621138cda853cdcce9faf0ac569e1.

src/termvisage/tui/keys.py
@@ -383,11 +383,12 @@ def key_bar_rows():
 
 def resize():
     adjust_bottom_bar()
-    main.grid_render_queue.put(None)  # Mark the start of a new grid
-    main.grid_change.set()
-    # Wait till GridRenderManager clears the cache
-    while main.grid_change.is_set():
-        pass
+    if main.grid_active.is_set():
+        main.grid_render_queue.put(None)  # Mark the start of a new grid
+        main.grid_change.set()
+        # Wait till GridRenderManager clears the cache
+        while main.grid_change.is_set():
+            pass
 
 
 keys["global"].update({"resized": [resize, True]})