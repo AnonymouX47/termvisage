AnonymouX47/termvisage

commit e3d49eae0a8a87c9443ef137fe7f41acb5f99606

refactor: Combine `--foo/--no-foo` CL options
- Change: Combine command-line options of the form `--foo, --no-foo`
  into one entry using `argparse.BooleanOptionalAction`.

  - `--multi, --no-multi`.
  - `--swap-win-size, --no-swap-win-size`.

src/termvisage/parsers.py
@@ -2,7 +2,13 @@
 
 import re
 import sys
-from argparse import Action, ArgumentParser, RawDescriptionHelpFormatter, _ArgumentGroup
+from argparse import (
+    Action,
+    ArgumentParser,
+    BooleanOptionalAction,
+    RawDescriptionHelpFormatter,
+    _ArgumentGroup,
+)
 
 from term_image.image import ITerm2Image, Size
 
@@ -219,23 +225,14 @@ cell_ratio_options.add_argument(
     ),
 )
 
-win_size_options = general.add_mutually_exclusive_group()
-win_size_options.add_argument(
+general.add_argument(
     "--swap-win-size",
-    action="store_true",
+    action=BooleanOptionalAction,
     default=None,
     help=(
-        "A workaround for *auto* :term:`cell ratio` on some terminal emulators "
-        "(e.g older VTE-based ones) that wrongly report window dimensions swapped"
-    ),
-)
-win_size_options.add_argument(
-    "--no-swap-win-size",
-    action="store_false",
-    default=None,
-    dest="swap_win_size",
-    help=(
-        "Unlike :option:`--swap-win-size`, use the reported window size as-is (default)"
+        "Enable/Disable a workaround for *auto* :term:`cell ratio` on some terminal "
+        "emulators (e.g older VTE-based ones) that wrongly report window dimensions "
+        "swapped"
     ),
 )
 
@@ -575,20 +572,11 @@ perf_options.add_argument(
         f"(default: {config_options.grid_renderers})"
     ),
 )
-
-multi_options = perf_options.add_mutually_exclusive_group()
-multi_options.add_argument(
+perf_options.add_argument(
     "--multi",
-    action="store_true",
+    action=BooleanOptionalAction,
     default=None,
-    help="Enable multiprocessing, if supported (default)",
-)
-multi_options.add_argument(
-    "--no-multi",
-    action="store_false",
-    default=None,
-    dest="multi",
-    help="Disable multiprocessing",
+    help="Enable (if supported) or disable multiprocessing (default: enabled)",
 )
 
 # Config

commit 90541f6ff8af1b9b780a8e16eaaed0976764ce63

fix: Fix `--foo/--no-foo` options on Python<3.9
- Fix: Handle `ImportError` on Python<3.9.
- Add: `argparse.BooleanOptionalAction` replica for Python<3.9.
  - `argparse.BooleanOptionalAction` was added in 3.9.

Broken in e3d49eae0a8a87c9443ef137fe7f41acb5f99606.

src/termvisage/parsers.py
@@ -2,13 +2,7 @@
 
 import re
 import sys
-from argparse import (
-    Action,
-    ArgumentParser,
-    BooleanOptionalAction,
-    RawDescriptionHelpFormatter,
-    _ArgumentGroup,
-)
+from argparse import Action, ArgumentParser, RawDescriptionHelpFormatter, _ArgumentGroup
 
 from term_image.image import ITerm2Image, Size
 
@@ -57,6 +51,20 @@ For other shells, see https://github.com/kislyuk/argcomplete/tree/develop/contri
 class BasicHelpAction(Action):
     def __call__(self, *args):
         basic_parser.parse_args(["--help"])
+
+
+try:
+    from argparse import BooleanOptionalAction
+except ImportError:  # Python < 3.9
+
+    class BooleanOptionalAction(Action):
+        def __init__(self, *args, **kwargs):
+            kwargs["nargs"] = 0
+            super().__init__(*args, **kwargs)
+            self.option_strings.append(f"--no-{self.option_strings[0][2:]}")
+
+        def __call__(self, parser, namespace, values, option_string):
+            setattr(namespace, self.dest, not option_string.startswith("--no-"))
 
 
 class CompletionsAction(Action):