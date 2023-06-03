112d1c07e696b966b6a90ded4ddbdcf73c62aa70
AnonymouX47 <anonymoux47@gmail.com>
2023-06-03 01:10:15 +0100

    chore: Bump package version to `0.1.0`

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="termvisage",
-    version="0.1.0-dev",
+    version="0.1.0",
     author="Toluwaleke Ogundipe",
     author_email="anonymoux47@gmail.com",
     url="https://github.com/AnonymouX47/termvisage",
