AnonymouX47/termvisage

commit 6ef2a51a0cb697d03dd643bda18a56f2d80a86e1

refac: tui: Improve grid render refresh on resize
- Change: Refresh grid rendering regardless of whether the grid is
  active or not.
- Change: For graphics-based styles, refresh grid rendering based on
  change in terminal cell size.

  Cell ratio is now only for text-based styles.

src/termvisage/tui/keys.py
@@ -394,9 +394,9 @@ def resize():
         # implement the `TIOCSWINSZ` ioctl command. Hence, the `cell_size and`.
         if cell_size and cell_size != _prev_cell_size:
             _prev_cell_size = cell_size
+            refresh_grid_rendering()
             Image._ti_update_grid_thumbnailing_threshold(cell_size)
-
-    if main.grid_active.is_set():
+    else:
         cell_ratio = get_cell_ratio()
         if cell_ratio != _prev_cell_ratio:
             _prev_cell_ratio = cell_ratio
@@ -737,6 +737,7 @@ _cancel: tuple[FunctionType, tuple[Any, ...]] | None = None
 _prev_view_widget: urwid.Widget | None = None
 
 # Used to guard clearing of grid render cache
+# Updated from `resize()`, for text-based styles.
 _prev_cell_ratio: float = 0.0
 
 # Used to [re]compute the thumbnailing threshold.

commit 611c93120040812730037cf97d18996ca2649c27

fix: tui.keys: Fix grid render resync upon resize
- Fix: Do not resync grid image rendering upon terminal resize if the
  grid is not active.

  Fixes a deadlock when the terminal is resized and cell size changes
  while the grid is not active.

Broken in 6ef2a51a0cb697d03dd643bda18a56f2d80a86e1.

src/termvisage/tui/keys.py
@@ -394,14 +394,16 @@ def resize():
         # implement the `TIOCSWINSZ` ioctl command. Hence, the `cell_size and`.
         if cell_size and cell_size != _prev_cell_size:
             _prev_cell_size = cell_size
-            resync_grid_rendering()
             if main.THUMBNAIL:
                 Image._ti_update_grid_thumbnailing_threshold(cell_size)
+            if main.grid_active.is_set():
+                resync_grid_rendering()
     else:
         cell_ratio = get_cell_ratio()
         if cell_ratio != _prev_cell_ratio:
             _prev_cell_ratio = cell_ratio
-            resync_grid_rendering()
+            if main.grid_active.is_set():
+                resync_grid_rendering()
 
     adjust_bottom_bar()
     getattr(main.ImageClass, "clear", lambda: True)() or ImageCanvas.change()